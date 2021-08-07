f4d286a41706823b6039f6ea6b647360028da2b5
xeniya <xeniia.izotowa@gmail.com>
2021-08-07 08:54:55 +0300

    Init commit

---
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,20 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+  /**
+   * Calculate index of max income
+   *
+   * @param incomes - array of incomes
+   * @return - index of first max value
+   */
+  public long findMax(long[] incomes) {
+    long currentMaxIndex = 0;
+    long currentMax = incomes[0];
+    for (long income : incomes) {
+      if (currentMax < income) {
+        currentMax = income;
+      }
+    }
+        return currentMax;
+  }
+}
